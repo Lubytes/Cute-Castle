5430ffad96cd49a99b03d6463c51d397d1463375
MiekuAlt <mc627781@dal.ca>
2017-11-02 00:19:43 -0300

    Implimented basic character movement as well as basic collision detection

---
+++ Pipe Castle/Assets/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *  Michael Altair 
+ *  This class controls the player character's movement
+ */
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+
+    public Vector3 velocity;
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = targetVelocityX;
+        transform.Translate(velocity * Time.deltaTime);
+
+    }
+}
