169cdaa372a4c9c0ad573614bf4f9e0203c9cff4
Sarah Young <sr943542@dal.ca>
2017-11-15 10:58:29 -0400

    Coin Counter Prefab
    Updates player's coin count and displays it on the GUI

---
+++ Pipe Castle/Assets/CoinCount.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CoinCount : MonoBehaviour {
+
+	public Sprite[] coinSprite;
+	public GameObject[] gameObjects;
+	public int numCoins;
+
+	// Use this for initialization
+	void Start () {
+
+		IncrementCoin ();
+		UpdateCoins ();
+
+}
+	void UpdateCoins(){
+		
+		var digits = new List<int>();
+		while (numCoins > 0)
+		{
+			digits.Add(numCoins % 10);
+			numCoins /= 10;
+		}
+
+		digits.Reverse();
+
+		int n = digits.Count;
+
+		for(int i = 0; i<n; i++){
+			gameObjects [i].GetComponent<SpriteRenderer> ().sprite = coinSprite [digits[i]];
+		}	
+
+}
+	public void IncrementCoin(){
+		numCoins++;
+		UpdateCoins ();
+		
+}
+
+}
