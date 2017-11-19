7564de388ca2f0058859fcbcc9581c6c4b39e4fb
Michael <m.altair@live.com>
2017-11-19 14:12:32 -0400

    Fixing the merge

---
+++ Pipe Castle/Assets/PlayerController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ *  Michael Altair 
+ *  This class controls the player character's movement
+ */
+
+public class PlayerController : MonoBehaviour {
+
+    private bool grounded;
+
+    public float moveSpeed;
+    public float jumpPower = 10;
+
+    public Vector3 velocity;
+
+    public bool isGrown;
+    public HeartsGUI hearts;
+    public CoinCount coinCount;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            PlayerMove(input);
+        }
+
+
+        // Detecting if the player is midair
+        if(gameObject.GetComponent<Rigidbody2D>().velocity.y == 0)
+        {
+            grounded = true;
+        } else
+        {
+            grounded = false;
+        }
+    }
+
+    public void PlayerMove(Vector2 input)
+    {
+        velocity.y = 0;
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = targetVelocityX;
+        transform.Translate(velocity * Time.deltaTime);
+
+        if(input.y > 0)
+        {
+            PlayerJump();
+        }
+    }
+
+    public void PlayerJump()
+    {
+        if (grounded)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector3(gameObject.GetComponent<Rigidbody2D>().velocity.x, jumpPower, 0), ForceMode2D.Impulse);
+        }
+    }
+
+    // Collision logic
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Monster")
+        {
+            Hurt();
+        } else if (other.gameObject.tag == "Power-Up")
+        {
+            PowerUp(other.gameObject);
+        }
+
+    }
+
+    // Triggers when the player is injured
+    public void Hurt()
+    {
+        Handheld.Vibrate();
+        hearts.DecreaseHeart();
+        if(hearts.numHearts == 0)
+        {
+            Death();
+        }
+    }
+
+    // Triggers on death
+    private void Death()
+    {
+        Debug.Log("You Dead!");
+    }
+
+    // Handles Powerups
+    private void PowerUp(GameObject powerUp)
+    {
+        if(powerUp.name == "Health-Up" || powerUp.name == "Health-Up(Clone)")
+        {
+            hearts.IncreaseHeart();
+        } else if(powerUp.name == "Coin" || powerUp.name == "Coin(Clone)")
+        {
+            coinCount.IncrementCoin();
+        }
+
+        // Destroys the powerup in the end
+        Destroy(powerUp);
+    }
+
+    // Makes the player recoil
+    void Recoil(Collider2D other)
+    {
+        int dirX, dirY;
+
+        if(gameObject.transform.position.x - other.transform.position.x < 0)
+        {
+            dirX = -1;
+        } else
+        {
+            dirX = 1;
+        }
+
+        if (gameObject.transform.position.y - other.transform.position.y < 0)
+        {
+            dirY = -1;
+        }
+        else
+        {
+            dirY = 1;
+        }
+
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(2.5f * dirX, 2.5f * dirY);
+    }
+}
