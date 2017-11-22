0f21e4cbb95d6cd41ab6996ee5d3b31ad5b39a28
Sarah Young <sr943542@dal.ca>
2017-11-22 08:46:59 -0400

    Pause Functionality
    A in-game FABB with pause functionality.

---
+++ Pipe Castle/Assets/PauseGame.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+	public Transform canvas;
+	// Update is called once per frame
+	void Update () {
+		//using escape key for now until toggle is put in game screen.
+		if(Input.GetKeyDown(KeyCode.Escape)){
+			Pause ();
+		}
+	}
+
+	public void Pause () {
+		if (canvas.gameObject.activeInHierarchy == false) {
+			canvas.gameObject.SetActive (true);
+			// pause time
+			Time.timeScale = 0;
+
+		} 
+		else {
+			canvas.gameObject.SetActive (false);
+			// set time back to normal
+			Time.timeScale = 1;
+		}
+	}
+}
