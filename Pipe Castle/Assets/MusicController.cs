54aacab80b674f07770837ecde7ec0c15bfb5ce1
Sarah Young <sr943542@dal.ca>
2017-12-01 15:38:00 -0400

    Script for changing music/sfx
    Script to turn music and sound effects on/off.

---
+++ Pipe Castle/Assets/MusicController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicController : MonoBehaviour {
+
+
+	public Toggle musicToggle;
+	public Toggle sfxToggle;
+
+	public void loadMusic(){
+	
+		if (musicToggle.isOn) {
+			//music on
+			Debug.Log ("yes");
+		} else if (!musicToggle.isOn) {
+			//music off
+			Debug.Log ("no");
+		}
+	}
+
+	public void loadSfx(){
+
+		if (sfxToggle.isOn) {
+			// sound effects on
+			Debug.Log ("yes");
+		} else if (!sfxToggle.isOn) {
+			// sound effects off
+			Debug.Log ("no");
+		}
+	}
+
+}
